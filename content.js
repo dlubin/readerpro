695fd8518f02edeb06f5cc3e3580bbf887383189
David Lubin <david@davidlubindigital.com>
2017-12-10 23:25:50 -0500

    v0.2.1 - In selecting mode, "p" selectors are highlighted in blue. Will cover additional scenarios later

--- content.js
+++ content.js
@@ -6,8 +6,7 @@ var state = "idle";
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if(request.message == "state_selecting"){
-      state = "selecting";
-      console.log("Now in selecting mode");      
+      selecting();      
     }
   }
 );
@@ -33,10 +32,46 @@ $(document).contextmenu(
   }
 );
 
-//Return to idle mode
+$(document).click(
+  function(){
+    if(state == "selecting"){
+
+    }
+  }
+)
+
+var hoverListener = function(){
+  originalBackground = typeof $(this).css("background-color") != "undefined" ? $(this).css("background-color") : "";
+  $(this).css("background-color","#87e3ff");
+}
+var hoverOffListener = function(){
+  if(typeof originalBackground != "undefined" && originalBackground != null){
+    $(this).css("background-color",originalBackground);
+    originalBackground = null;
+  }
+}
+
+//Set to "selecting" mode
+function selecting() {
+  state = "selecting";
+  //Prevent repeated bindings
+  $("p").unbind("mouseover",hoverListener);
+  $("p").unbind("mouseout",hoverOffListener);
+  //Update CSS so that text background becomes blue ("#87e3ff") on hover
+  $("p").bind("mouseover",hoverListener);
+  $("p").bind("mouseout",hoverOffListener);
+
+  console.log("ReaderPro - Now in selecting mode");
+}
+
+//Set to "idle" mode
 function idle(){
   chrome.runtime.sendMessage({"message":"state_idle"}, function(response){
     state = "idle";
-    console.log("Now in idle mode");
+    $("p").unbind("mouseover",hoverListener);
+    //Don't unbind mouseout to ensure that if something was already highlighted, 
+    //it still gets unhighlighted on mouseout
+
+    console.log("ReaderPro - Now in idle mode");
   });
 }
